98905fbbbcf7320492debe393ce5d2dd1ff2fdd5
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-08-16 13:53:45 +0300

    fetch add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/name/{name}?fullText=true')
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+  })
+  .catch(error => {
+    console.log(error);
+  });
